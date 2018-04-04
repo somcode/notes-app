ec2978817d5801696e72f01721e4848cde8b908c
Somayeh <mail.somayeh@gmail.com>
2018-04-04 15:00:59 +0100

    improved test code with using assert

--- note-spec.js
+++ note-spec.js
@@ -1,23 +1,37 @@
-(function(exports) {
+// (function(exports) {
+//
+//   function testNoteReturnNote() {
+//     var note = new Note("hello");
+//
+//     if (note.text !== "hello") {
+//       throw new Error("text hello, should return hello");
+//     }
+//   };
+//   testNoteReturnNote();
+//
+//
+//   function testViewNoteReturnNote() {
+//     var note = new Note("hello world");
+//
+//     if (note.viewNote() !== "hello world") {
+//       throw new Error("viewNote hello world, should return hello world");
+//     }
+//   };
+//
+//   testViewNoteReturnNote();
+//
+// })(this);
 
-  function testNoteReturnNote() {
-    var note = new Note("hello");
 
-    if (note.text !== "hello") {
-      throw new Error("text hello, should return hello");
-    }
-  };
-  testNoteReturnNote();
+function testNoteReturnNote() {
+  var note = new Note("hello");
+  assert.isTrue(note.text === "hello");
+};
+testNoteReturnNote();
 
 
-  function testViewNoteReturnNote() {
-    var note = new Note("hello world");
-
-    if (note.viewNote() !== "hello world") {
-      throw new Error("viewNote hello world, should return hello world");
-    }
-  };
-
-  testViewNoteReturnNote();
-
-})(this);
+function testViewNoteReturnNote() {
+  var note = new Note("hello world");
+  assert.isTrue(note.viewNote() === "hello world")
+};
+testViewNoteReturnNote();
